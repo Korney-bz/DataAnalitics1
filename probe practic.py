84fcde023928cdf623f9b0d4970a9f8b6457cfb1
Mikhail <93600647+Korney-bz@users.noreply.github.com>
2022-09-27 12:06:24 +0300

    practic 2
    27/09/22

--- probe practic.py
+++ probe practic.py
@@ -1,3 +1,8 @@
+from random import randint
+
+from termcolor import cprint
+
+
 class Man:
     def __init__(self, name):
         self.name = name
@@ -7,12 +12,13 @@ class Man:
 
     def __str__(self):
         return (
-            'Я - {}, Сытость - {}, '
+            'Я - {}, '
+            'Сытость - {}, '
             'Осталось еды - {}, '
             'Осталось денег - {}'.format(self.name, self.fullness, self.food, self.money))
 
     def eat(self):
-        if self.food > 10:
+        if self.food >= 10:
             print('{} поел'.format(self.name))
             self.fullness += 10
             self.food -= 10
@@ -20,25 +26,42 @@ class Man:
             print('{} нет еды'.format(self.name))
 
     def work(self):
-        print('{} сходил на работу'.format(self.name))
+        cprint('{} сходил на работу'.format(self.name), color='grey')
         self.money += 50
 
-    def play_DOTA(self):
-        print('{} слил катку обезьяна'.format(self.name))
+    def play_APEX(self):
+        cprint('{} играет в Apex Legends'.format(self.name), color='green')
         self.fullness -= 15
 
-    def sleep(self):
-        print('{} выспался'.format(self.name))
-        self.fullness -= 15
+    def shopping(self):
+        if self.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='white')
+            self.money -= 50
+            self.food += 50
+        else:
+            print('{} деньги закончились'.format(self.name))
+
+    def act(self):
+        if self.fullness <= 0:
+            print('{} еды нет, Вася отъехать может'.format(self.name))
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.food < 10:
+            self.shopping()
+        elif self.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.play_APEX()
 
 
-vasya = Man(name='Vasya')
-print(vasya)
-vasya.eat()
-print(vasya)
-vasya.work()
-print(vasya)
-vasya.play_DOTA()
-print(vasya)
-vasya.sleep()
-print(vasya)
+dima = Man(name='Dima')
+for day in range(1, 21):
+    cprint('================== день {} ================='.format(day), color='yellow')
+    dima.act()
+    print(dima)
